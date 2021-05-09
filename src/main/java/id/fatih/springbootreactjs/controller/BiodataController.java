f29d2ed6789c79883eeed311676b19423646d924
Khoerul Fatihin <khoerulfatihin1011@gmail.com>
2021-05-09 13:46:33 +0700

    test auto1

--- src/main/java/id/fatih/springbootreactjs/controller/BiodataController.java
+++ src/main/java/id/fatih/springbootreactjs/controller/BiodataController.java
@@ -2,9 +2,7 @@ package id.fatih.springbootreactjs.controller;
 
 import id.fatih.springbootreactjs.model.DefaultResponse;
 import id.fatih.springbootreactjs.model.dto.BiodataDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/biodata")
@@ -21,5 +19,12 @@ public class BiodataController {
     @GetMapping("/geterror")
     public DefaultResponse <BiodataDto> getBiodataError(){
         return DefaultResponse.error("data hilang");
+    }
+    @PostMapping("/save")
+    public DefaultResponse <BiodataDto> saveBiodata (@RequestBody BiodataDto biodataDto){
+        BiodataDto bio = new BiodataDto();
+        bio.setNama("alfath");
+        bio.setAlamat("kebumen");
+        return DefaultResponse.ok(biodataDto);
     }
 }
